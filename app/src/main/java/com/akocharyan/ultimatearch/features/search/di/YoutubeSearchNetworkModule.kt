b9cc0432543576c45f65f11aa5f3dc49129bc99d
AramKocharyan <aram.kocharian@gmail.com>
2022-06-19 21:01:10 +0400

    Improved build

--- app/src/main/java/com/akocharyan/ultimatearch/features/search/di/YoutubeSearchNetworkModule.kt
+++ app/src/main/java/com/akocharyan/ultimatearch/features/search/di/YoutubeSearchNetworkModule.kt
@@ -1,21 +1,8 @@
 package com.akocharyan.ultimatearch.features.search.di
 
-import com.google.gson.GsonBuilder
-import com.google.gson.reflect.TypeToken
-import com.akocharyan.core.network.PagedData
-import com.akocharyan.ultimatearch.features.search.data.YoutubeSearchRepository
-import com.akocharyan.ultimatearch.features.search.data.YoutubeSearchRepositoryImpl
-import com.akocharyan.ultimatearch.features.search.data.YoutubeSearchService
-import com.akocharyan.ultimatearch.features.search.data.YoutubeVideoTypeAdapter
-import com.akocharyan.ultimatearch.features.search.domain.models.YoutubeVideoDto
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
