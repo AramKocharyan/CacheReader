bd70c038e02349d85ce8359c0780497bc0745c6b
AramKocharyan <aram.kocharian@gmail.com>
2022-06-27 15:14:59 +0400

    Added fragment and viewModel

---
+++ features/home/src/main/java/com/akocharyan/home/ui/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.akocharyan.home.ui
+
+import androidx.fragment.app.viewModels
+import com.akocharyan.core.platorm.BaseFragment
+
+class HomeFragment : BaseFragment() {
+
+    private val viewModel by viewModels<HomeViewModel>()
+
+
+}
---
+++ features/home/src/main/java/com/akocharyan/home/ui/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.akocharyan.home.ui
+
+import com.akocharyan.core.platorm.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class HomeViewModel : BaseViewModel()
